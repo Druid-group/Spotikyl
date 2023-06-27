2ce0c8f084ae66726d34775534e030b163d1bd8f
k-mabbott <kmabbott9@gmail.com>
2023-06-27 13:49:30 -0700

    initial commit of react package

---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import useAuth from './useAuth'
+import SpotifyWebApi from 'spotify-web-api-node'
+import SpotifyPlayer from 'react-spotify-web-playback'
+
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: '7c7ec56729db4416b88c933966532ad3'
+})
+
+
+const Dashboard = ({ code }) => {
+
+    const accessToken = useAuth(code)
+    const [tracks, setTracks] = useState();
+    const [playingTrack, setPlayingTrack] = useState('spotify:track:4vTXBC7QOjEbi8DcJvCNE2');
+
+    useEffect(() => {
+        if (!accessToken) return
+        spotifyApi.setAccessToken(accessToken)
+    }, [tracks])
+
+    const getSongs = () => {
+        spotifyApi.searchTracks('jump').then(res => {
+            console.log(res.body.tracks.items) 
+            setTracks(res.body.tracks.items)
+            setPlayingTrack(res.body.tracks.items[0].id)
+        } )
+    }
+
+
+
+    console.log(playingTrack, '--------------------------')
+
+    return (
+        <>
+        {/* <div>Dashboard {accessToken} </div> */}
+        <button onClick={getSongs} >Do it</button>
+        {
+            tracks? tracks.map((track, i) => 
+            <div>
+                <p>{track.name}</p>
+            </div>) : <></>
+        }
+        {/* { accessToken &&
+        <SpotifyPlayer token={accessToken} uris={[playingTrack]} /> 
+        } */}
+        <iframe style={{borderRadius: '12px'}} src={`https://open.spotify.com/embed/track/${playingTrack}?utm_source=generator`} width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </>
+    )
+}
+
+export default Dashboard
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AuthPage = () => {
+
+    const redirect_uri = 'http://localhost:3000'
+    const client_id = '7c7ec56729db4416b88c933966532ad3'
+    const scope = 'streaming%20user-read-email%20user-read-private%20user-modify-playback-state%20user-read-playback-state%20user-library-read%20user-library-modify'
+
+    const authorize = 'https://accounts.spotify.com/authorize'
+
+
+
+    const auth_url = `${authorize}?client_id=${client_id}&response_type=code&redirect_uri=${encodeURI(redirect_uri)}&show_dialog=true&scope=${scope}`
+
+
+
+
+    return (
+        <div>
+            <h3>Click to Authorize</h3>
+            <a href={auth_url} className='' >Authorize</a>
+        </div>
+    )
+}
+
+export default AuthPage
